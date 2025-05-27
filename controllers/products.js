5f62c3eee2af3cb67714ff7f6aecc1782fb82e80
Robin Singh Chaudhary <124763217+robinn007@users.noreply.github.com>
2025-05-27 15:26:26 +0000

    initial commit

---
+++ controllers/products.js
@@ -0,0 +1,31 @@
+const Product = require("../models/product")
+
+const getAllProducts = async(req, res) => {
+    const { company, name } = req.query;
+    const queryObject = {};
+
+    if (company) {  
+        queryObject.company = company;  
+    }
+
+    if(name){
+        queryObject.name = { $regex: name, $options: 'i' };
+    }
+
+    if(featured){
+        queryObject.featured = featured;
+    }
+
+    console.log(queryObject);
+
+    const myData = await Product.find(queryObject);
+    res.status(200).json({ myData });
+}
+
+const getAllProductsTesting = async(req, res) => {
+   // res.status(200).json({ msg : "I am getAllProductsTesting" });
+    const myData = await Product.find(req.query).sort("name");
+    res.status(200).json({ myData });
+}
+
+module.exports = {getAllProducts, getAllProductsTesting}; 
